465f049821f344b379f03a7f95e3e84d46a28ce4
Pankaj Agarwal <quasar.pankaj@gmail.com>
2022-09-07 18:01:51 +0530

    Added Recursive Descent Parser for parsing nodes

--- lib/services/conflict_wrapper.dart
+++ lib/services/conflict_wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:plot_generator/services/parser.dart';
 import 'package:plot_generator/services/plotto.dart';
 
 import 'master_conflict.dart';
@@ -10,22 +11,55 @@ class ConflictWrapper {
     if (_conflict.description is String) {
       return _conflict.description;
     } else if (_conflict.description is List) {
-      String desc = '';
-      for (var element in _conflict.description) {
-        if (element is String) {
-          desc += element;
-        } else if (element is List) {
-          final plotto = Plotto.getInstance();
-          String linkDesc = '';
-          for (var link in element) {
-            MasterConflict c = plotto.conflicts[link]!;
-            ConflictWrapper w = ConflictWrapper(conflict: c);
-            linkDesc += '\n' + w.description;
-          }
-          desc += '\n' + linkDesc;
-        } else if (element is Map) {}
-      }
-      return desc;
+      return _processList(_conflict.description);
+    } else {
+      _throwError();
     }
+    return '';
+  }
+
+  String _processList(List<dynamic> descriptions) {
+    String desc = '';
+    for (var element in descriptions) {
+      if (element is String) {
+        desc += element;
+      } else if (element is List) {
+        return _processLinks(List.from(element));
+      } else {
+        _throwError();
+      }
+    }
+
+    return desc;
+  }
+
+  String _processMap(Map<dynamic, dynamic> element) {
+    final Parser parser = Parser();
+    parser.start(Map.from(element));
+    return parser.description;
+  }
+
+  void _throwError() {}
+
+  String _processLinks(List<dynamic> element) {
+    String desc = '';
+    for (var node in element) {
+      if (node is List) {
+      } else if (node is String) {
+        desc += _fetchConflict(node);
+      } else if (node is Map) {
+        desc += _processMap(node);
+      } else {
+        _throwError();
+      }
+    }
+    return desc;
+  }
+
+  String _fetchConflict(String node) {
+    final Plotto plotto = Plotto.getInstance();
+    final MasterConflict conflict = plotto.fetchConflictById(node);
+    final ConflictWrapper wrapper = ConflictWrapper(conflict: conflict);
+    return wrapper.description;
   }
 }
---
+++ lib/services/parser.dart
@@ -0,0 +1,95 @@
+import 'package:plot_generator/services/conflict_wrapper.dart';
+import 'package:plot_generator/services/master_conflict.dart';
+import 'package:plot_generator/services/plotto.dart';
+import 'package:plot_generator/services/random_mixin.dart';
+
+class Parser with RandomMixin {
+  String _description = '';
+  List<String> _links = [];
+
+  String get description => _description;
+  List<String> get links => _links;
+
+  void start(Map<String, dynamic> node) {
+    Map<String, dynamic> copy = Map.from(node);
+    _processMap(copy);
+  }
+
+  void _processMap(Map<String, dynamic> node) {
+    if (node.containsKey('op')) {
+      _parseOp(node);
+    }
+    if (node.containsKey('tfm')) {
+      _processTransform(Map.from(node));
+    }
+    if (node.containsKey('start')) {
+      _processStart(node);
+    }
+    if (node.containsKey('v')) {
+      _parseV(node['v']);
+    }
+  }
+
+  void _parseOp(Map<String, dynamic> node) {
+    String op = node['op'];
+    dynamic v = node['v'];
+    node.removeWhere((key, value) => key == 'op' || key == 'v');
+    switch (op) {
+      case '?':
+        _processChoose(v as List<dynamic>);
+        break;
+      case '+':
+        _processInclude(v);
+        break;
+    }
+  }
+
+  void _parseV(dynamic v) {
+    if (v is List) {
+      _processList(v);
+    }
+    if (v is String) {
+      _processString(v);
+    }
+    if (v is Map) {
+      _processMap(Map.from(v));
+    }
+  }
+
+  void _processList(List v) {
+    for (var element in v) {
+      _parseV(element);
+    }
+  }
+
+  void _processString(String v) {
+    final Plotto plotto = Plotto.getInstance();
+    final MasterConflict conflict = plotto.fetchConflictById(v);
+    final ConflictWrapper wrapper = ConflictWrapper(conflict: conflict);
+    _description += wrapper.description;
+  }
+
+  void _processChoose(List<dynamic> v) {
+    final rnd = getRandom(v.length);
+    final chosen = v[rnd];
+    _parseV(chosen);
+  }
+
+  void _processInclude(List<dynamic> v) {
+    for (var link in v) {
+      _parseV(link);
+    }
+  }
+
+  void _processTransform(Map<String, String> node) {}
+
+  void _processStart(Map<String, dynamic> node) {
+    final String start = node['start'];
+    late final String end;
+    if (node.containsKey('end')) {
+      end = node['end'];
+    }
+  }
+
+  void _processListDescription(description) {}
+}
