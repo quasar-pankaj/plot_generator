5dc318f1488b6d8fa4b934da6eb6d77162fad92b
Pankaj Agarwal <quasar.pankaj@gmail.com>
2024-03-28 16:11:12 +0530

    rebuilt the platforms

---
+++ android/app/src/main/kotlin/com/example/plot_generator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.plot_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/pankaj/plot_generator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pankaj.plot_generator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
